798f51eb91329b7537cae2a9807d81cd74326bdc
George Perera <george.perera@noc.ac.uk>
2025-01-17 12:26:00 +0000

    updated with JSON reader

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -21,6 +21,21 @@ class CSVDataSource:
         data = map(models.load_csv, data_file_paths)
         return list(data)
 
+class JSONDataSource:
+  """
+  Loads patient data with inflammation values from JSON files within a specified folder.
+  """
+  def __init__(self, dir_path):
+    self.dir_path = dir_path
+
+  def load_inflammation_data(self):
+    data_file_paths = glob.glob(os.path.join(self.dir_path, 'inflammation*.json'))
+    if len(data_file_paths) == 0:
+      raise ValueError(f"No inflammation JSON files found in path {self.dir_path}")
+    data = map(models.load_json, data_file_paths)
+    return list(data)
+
+
 def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
